d1961f98b8c72289954f3dfb2ffb4d782453f631
Jean-André Santoni <jean.andre.santoni@gmail.com>
2014-08-25 19:15:44 +0200

    (Lakka) add 96x96 icons

--- lakka/monochrome/src/export.sh
+++ lakka/monochrome/src/export.sh
@@ -1,7 +1,8 @@
-for dim in 64 128 192 256; do
+for dim in 64 96 128 192 256; do
 	for src in *.svg; do
 		echo $src;
 		export dest=`echo $src | sed "s/.svg/.png/"`
+		mkdir -p ../$dim
 		inkscape -z -C -w $dim -h $dim -f "$src" -e "../$dim/$dest"
 	done
 done
