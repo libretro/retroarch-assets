4a56f701ba0fee2249817e0d90510801d61632fb
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-02-16 16:05:23 +0100

    (XMB) Add ZX Spectrum icons

--- lakka/monochrome/src/export.sh
+++ lakka/monochrome/src/export.sh
@@ -4,5 +4,9 @@ for dim in 256; do
 		export dest=`echo $src | sed "s/.svg/.png/"`
 		mkdir -p ../$dim
 		inkscape -z -C -w $dim -h $dim -f "$src" -e "../$dim/$dest"
+		#rsvg-convert -b none -w $dim -h $dim "$src" -o "../$dim/$dest"
+		#convert -density 360 -background none "$src" "../$dim/$dest"
+		#convert "../$dim/$dest" -write mpr:temp -background black -alpha Remove mpr:temp -compose Copy_Opacity -composite "../$dim/$dest"
+		#python2 ../../NPMApng2PMApng.py "../$dim/$dest" "../$dim/$dest"
 	done
 done
