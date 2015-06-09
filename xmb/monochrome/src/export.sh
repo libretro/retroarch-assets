68743885f2b8fc3986cbd0b3df69953f37bda174
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-06-09 14:21:37 +0700

    Change the export script to reflect the new paths

--- xmb/monochrome/src/export.sh
+++ xmb/monochrome/src/export.sh
@@ -1,12 +1,11 @@
-for dim in 256; do
-	for src in *.svg; do
-		echo $src;
-		export dest=`echo $src | sed "s/.svg/.png/"`
-		mkdir -p ../$dim
-		inkscape -z -C -w $dim -h $dim -f "$src" -e "../$dim/$dest"
-		#rsvg-convert -b none -w $dim -h $dim "$src" -o "../$dim/$dest"
-		#convert -density 360 -background none "$src" "../$dim/$dest"
-		#convert "../$dim/$dest" -write mpr:temp -background black -alpha Remove mpr:temp -compose Copy_Opacity -composite "../$dim/$dest"
-		#python2 ../../NPMApng2PMApng.py "../$dim/$dest" "../$dim/$dest"
-	done
+for src in *.svg; do
+	echo $src;
+	export dest=`echo $src | sed "s/.svg/.png/"`
+	mkdir -p ../png
+	inkscape -z -C -w 256 -h 256 -f "$src" -e "../png/$dest"
+	#rsvg-convert -b none -w $dim -h $dim "$src" -o "../$dim/$dest"
+	#convert -density 360 -background none "$src" "../$dim/$dest"
+	#convert "../$dim/$dest" -write mpr:temp -background black -alpha Remove mpr:temp -compose Copy_Opacity -composite "../$dim/$dest"
+	#python2 ../../NPMApng2PMApng.py "../$dim/$dest" "../$dim/$dest"
 done
+
