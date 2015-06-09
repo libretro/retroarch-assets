00a589795fcccfed9a0b84383d9aa4c1a0d9d8cb
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-06-09 14:47:11 +0700

    Change the export script to reflect the new paths

--- xmb/monochrome/src/export.sh
+++ xmb/monochrome/src/export.sh
@@ -3,9 +3,9 @@ for src in *.svg; do
 	export dest=`echo $src | sed "s/.svg/.png/"`
 	mkdir -p ../png
 	inkscape -z -C -w 256 -h 256 -f "$src" -e "../png/$dest"
-	#rsvg-convert -b none -w $dim -h $dim "$src" -o "../$dim/$dest"
-	#convert -density 360 -background none "$src" "../$dim/$dest"
-	#convert "../$dim/$dest" -write mpr:temp -background black -alpha Remove mpr:temp -compose Copy_Opacity -composite "../$dim/$dest"
-	#python2 ../../NPMApng2PMApng.py "../$dim/$dest" "../$dim/$dest"
+	#rsvg-convert -b none -w 256 -h 256 "$src" -o "../png/$dest"
+	#convert -density 360 -background none "$src" "../png/$dest"
+	#convert "../png/$dest" -write mpr:temp -background black -alpha Remove mpr:temp -compose Copy_Opacity -composite "../png/$dest"
+	#python2 ../../NPMApng2PMApng.py "../png/$dest" "../png/$dest"
 done
 
