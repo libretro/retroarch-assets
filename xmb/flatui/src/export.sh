976f0496c5f62b74814474a80050fb1efd424759
Jean-André Santoni <jean.andre.santoni@gmail.com>
2015-06-09 15:49:42 +0700

    Fix export script for flatui

--- xmb/flatui/src/export.sh
+++ xmb/flatui/src/export.sh
@@ -1,8 +1,11 @@
-for dim in 256; do
-	for src in *.svg; do
-		echo $src;
-		export dest=`echo $src | sed "s/.svg/.png/"`
-		mkdir -p ../$dim
-		inkscape -z -C -w $dim -h $dim -f "$src" -e "../$dim/$dest"
-	done
+for src in *.svg; do
+	echo $src;
+	export dest=`echo $src | sed "s/.svg/.png/"`
+	mkdir -p ../png
+	inkscape -z -C -w 256 -h 256 -f "$src" -e "../png/$dest"
+	#rsvg-convert -b none -w 256 -h 256 "$src" -o "../png/$dest"
+	#convert -density 360 -background none "$src" "../png/$dest"
+	#convert "../png/$dest" -write mpr:temp -background black -alpha Remove mpr:temp -compose Copy_Opacity -composite "../png/$dest"
+	#python2 ../../NPMApng2PMApng.py "../png/$dest" "../png/$dest"
 done
+
