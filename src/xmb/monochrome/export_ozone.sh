59dcbd1a4b28ef439ab7aa34dbb95b4c97c885e8
natinusala <natinusala@gmail.com>
2018-11-06 14:58:55 +0100

    Update ozone icons & export script

--- src/xmb/monochrome/export_ozone.sh
+++ src/xmb/monochrome/export_ozone.sh
@@ -7,3 +7,21 @@ for src in *.svg; do
 	optipng -o7 -strip all "../../../ozone/png/icons/$dest"
 done
 
+# Override some icons (different size)
+src="clock.svg"
+
+export dest=`echo $src | sed "s/.svg/.png/"`
+mkdir -p ../../../ozone/png/icons
+inkscape -z -C -w 92 -h 92 -f "$src" -e "../../../ozone/png/icons/$dest"
+convert "../../../ozone/png/icons/$dest" \( -clone 0 -fill "#FFFFFF" -draw "color 0,0 reset" \) -compose atop -composite "../../../ozone/png/icons/$dest"
+optipng -o7 -strip all "../../../ozone/png/icons/$dest"
+
+for src in battery-*.svg; do
+        export dest=`echo $src | sed "s/.svg/.png/"`
+        mkdir -p ../../../ozone/png/icons
+        inkscape -z -C -w 92 -h 92 -f "$src" -e "../../../ozone/png/icons/$dest"
+        convert "../../../ozone/png/icons/$dest" \( -clone 0 -fill "#FFFFFF" -draw "color 0,0 reset" \) -compose atop -composite "../../../ozone/png/icons/$dest"
+        optipng -o7 -strip all "../../../ozone/png/icons/$dest"
+done
+
+
