09222594aa6b337077e4467a4e8977c935037c28
natinusala <natinusala@gmail.com>
2018-11-15 18:42:50 +0100

    Update ozone assets

--- src/xmb/monochrome/export_ozone.sh
+++ src/xmb/monochrome/export_ozone.sh
@@ -1,8 +1,27 @@
 for src in *.svg; do
-	echo $src;
+        fletter="$(echo $src | head -c 1)"
+        
+        # Sidebar console tabs should be 40x40 (start with uppercase and doesn't end with -content)
+        # others should be 46x46
+        
+        case $src in
+                *-content.svg)
+                        size="46"
+                ;;
+                *)
+                        if [ "$fletter" == "${fletter,,}" ]; then
+	                        size="46"
+	                else
+	                        size="40"
+	                fi
+	        ;;
+        esac
+	
+	echo "Exporting $src ($size x $size)"
+	
 	export dest=`echo $src | sed "s/.svg/.png/"`
 	mkdir -p ../../../ozone/png/icons
-	inkscape -z -C -w 46 -h 46 -f "$src" -e "../../../ozone/png/icons/$dest"
+	inkscape -z -C -w $size -h $size -f "$src" -e "../../../ozone/png/icons/$dest"
 	convert "../../../ozone/png/icons/$dest" \( -clone 0 -fill "#FFFFFF" -draw "color 0,0 reset" \) -compose atop -composite "../../../ozone/png/icons/$dest"
 	optipng -o7 -strip all "../../../ozone/png/icons/$dest"
 done
